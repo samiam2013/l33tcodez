af7c2ab29fe4cb00e3c0baa1e48ab3abb24cd5d7
Samuel <samiam2013@gmail.com>
2022-02-17 09:42:49 +0000

    add test for addTwoNumbers

---
+++ easy/AddTwoNumbers_test.go
@@ -0,0 +1,144 @@
+package easy
+
+import (
+	"reflect"
+	"testing"
+)
+
+func Test_addTwoNumbers(t *testing.T) {
+	type args struct {
+		l1 *ListNode
+		l2 *ListNode
+	}
+	tests := []struct {
+		name string
+		args args
+		want *ListNode
+	}{
+		// TODO: Add test cases.
+		{
+			name: "first case 243+564=708",
+			args: args{
+				l1: &ListNode{
+					Val: 2,
+					Next: &ListNode{
+						Val: 4,
+						Next: &ListNode{
+							Val:  3,
+							Next: nil,
+						},
+					},
+				},
+				l2: &ListNode{
+					Val: 5,
+					Next: &ListNode{
+						Val: 6,
+						Next: &ListNode{
+							Val:  4,
+							Next: nil,
+						},
+					},
+				},
+			},
+			want: &ListNode{
+				Val: 7,
+				Next: &ListNode{
+					Val: 0,
+					Next: &ListNode{
+						Val:  8,
+						Next: nil,
+					},
+				},
+			},
+		},
+		{
+			name: "0+0=0",
+			args: args{
+				l1: &ListNode{
+					Val:  0,
+					Next: nil,
+				},
+				l2: &ListNode{
+					Val:  0,
+					Next: nil,
+				},
+			},
+			want: &ListNode{
+				Val:  0,
+				Next: nil,
+			},
+		},
+		{
+			name: "9999999+9999=89990001",
+			args: args{
+				l1: &ListNode{
+					Val: 9,
+					Next: &ListNode{
+						Val: 9,
+						Next: &ListNode{
+							Val: 9,
+							Next: &ListNode{
+								Val: 9,
+								Next: &ListNode{
+									Val: 9,
+									Next: &ListNode{
+										Val: 9,
+										Next: &ListNode{
+											Val:  9,
+											Next: nil,
+										},
+									},
+								},
+							},
+						},
+					},
+				},
+				l2: &ListNode{
+					Val: 9,
+					Next: &ListNode{
+						Val: 9,
+						Next: &ListNode{
+							Val: 9,
+							Next: &ListNode{
+								Val:  9,
+								Next: nil,
+							},
+						},
+					},
+				},
+			},
+			want: &ListNode{
+				Val: 8,
+				Next: &ListNode{
+					Val: 9,
+					Next: &ListNode{
+						Val: 9,
+						Next: &ListNode{
+							Val: 9,
+							Next: &ListNode{
+								Val: 0,
+								Next: &ListNode{
+									Val: 0,
+									Next: &ListNode{
+										Val: 0,
+										Next: &ListNode{
+											Val:  1,
+											Next: nil,
+										},
+									},
+								},
+							},
+						},
+					},
+				},
+			},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := addTwoNumbers(tt.args.l1, tt.args.l2); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("addTwoNumbers() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
