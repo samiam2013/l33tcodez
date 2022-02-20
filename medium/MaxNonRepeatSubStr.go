b3149e2da347892c5f699fb2a5cd85a9da0c13ee
Samuel <samiam2013@gmail.com>
2022-02-20 20:49:55 +0000

    90th percentile for speed, 80th for memory footprint

--- medium/MaxNonRepeatSubStr.go
+++ medium/MaxNonRepeatSubStr.go
@@ -5,20 +5,35 @@ package medium
 
 func lengthOfLongestSubstring(s string) int {
 	uniquesPos := map[rune]int{}
-	maxUnique := 0
-	mostRecentUnique := 0
-	for strIdx, curRune := range []rune(s) {
+	var maxUniqueLen int
+	leastRecentUnique := 0
+	for strIdx, curRune := range s {
 		if prevPos, ok := uniquesPos[curRune]; ok {
 			// need to pop off every rune in the map that occurs before existingPos
-			for i := prevPos; i >= mostRecentUnique; i-- {
+			for i := leastRecentUnique; i < prevPos; i++ {
 				delete(uniquesPos, rune(s[i]))
 			}
-			mostRecentUnique = prevPos + 1
+			leastRecentUnique = prevPos + 1
 		}
 		uniquesPos[curRune] = strIdx
-		if len(uniquesPos) > maxUnique {
-			maxUnique = len(uniquesPos)
-		}
+		maxUniqueLen = maxOf(maxUniqueLen, len(uniquesPos))
 	}
-	return maxUnique
+	return maxUniqueLen
 }
+
+func maxOf(a, b int) int {
+	if a > b {
+		return a
+	}
+	return b
+}
+
+// func maxOf(nums ...int) int {
+// 	var max int
+// 	for _, val := range nums {
+// 		if (val > max) {
+// 			max = val
+// 		}
+// 	}
+// 	return max
+// }
