fefbc99d0d7afb7d782ca1d34842c98d151c7485
Samuel <samiam2013@gmail.com>
2022-02-20 18:11:00 +0000

    moved AddTwoNumbers and prompt for Longest Non-Repeating SubString

---
+++ medium/AddTwoNumbers.go
@@ -0,0 +1,45 @@
+package medium
+
+// ListNode is a Definition for singly-linked list. */
+type ListNode struct {
+	Val  int
+	Next *ListNode
+}
+
+func addTwoNumbers(l1 *ListNode, l2 *ListNode) *ListNode {
+	output := &ListNode{}
+	// have to copy the output pointer to build it without incrementing it
+	nextNode := output
+	nextNodeExists := true
+	for nextNodeExists {
+		nextNodeExists = false // false until proven true
+
+		sumOfNodes := l1.Val + l2.Val
+		if sumOfNodes > 9 {
+			carry := (sumOfNodes - (sumOfNodes % 10)) / 10
+			if l1.Next != nil {
+				l1.Next.Val += carry
+			} else if l2.Next != nil {
+				l2.Next.Val += carry
+			} else {
+				l1.Next = &ListNode{Val: carry, Next: nil}
+			}
+		}
+		nextNode.Next = &ListNode{Val: sumOfNodes % 10, Next: nil}
+		nextNode = nextNode.Next
+
+		if l1.Next != nil || l2.Next != nil {
+			// ^ proof that sequence needs to continue
+			nextNodeExists = true
+			if l1.Next == nil {
+				l1.Next = &ListNode{Val: 0, Next: nil}
+			}
+			if l2.Next == nil {
+				l2.Next = &ListNode{Val: 0, Next: nil}
+			}
+			l1 = l1.Next
+			l2 = l2.Next
+		}
+	}
+	return output.Next
+}
---
+++ medium/MaxNonRepeatSubStr.go
@@ -0,0 +1,9 @@
+package medium
+
+
+// 3. Longest Substring Without Repeating Characters
+// Given a string s, find the length of the longest substring without repeating characters.
+
+func lengthOfLongestSubstring(s string) int {
+	return 0
+}
