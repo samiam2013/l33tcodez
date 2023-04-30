1c089d734395e79fc7a234c77b9f977cc16b279e
samiam2013 <sam@myres.dev>
2023-04-30 21:10:40 +0000

    top 80% speed, top 99.67% mem

--- hard/4_MedianOfSortedLists.go
+++ hard/4_MedianOfSortedLists.go
@@ -1,143 +1,99 @@
 package hard
 
 import (
-	"fmt"
+	"math"
 )
 
 func findMedianSortedArrays(nums1 []int, nums2 []int) float64 {
-	var appended []int
-
-	listRanges := map[string]int{}
-	if len(nums1) <= 0 {
-		// nums2 must be the whole list
-		appended = nums2
-		fmt.Println("appended (just nums2)", appended)
+	if len(nums1) == 0 {
+		return median(nums2)
+	}
+	if len(nums2) == 0 {
+		return median(nums1)
 	}
 
-	if len(nums2) <= 0 {
-		// nums1 must be the whole list
-		appended = nums1
-		fmt.Println("appended (just nums1)", appended)
+	if nums1[0] > nums2[len(nums2)-1] {
+		return median(append(nums2, nums1...))
+	}
+	if nums2[0] > nums1[len(nums1)-1] {
+		return median(append(nums1, nums2...))
 	}
 
-	if appended == nil {
-		if listRanges["n1end"] > listRanges["n2start"] {
-			fmt.Println("branch 1: n1 -> n2")
-			// the lists overlap and need to be merged nums1[:(?n2start-1)] , (mix) , nums2[(?n1end+1):]
-			// find the nums 2 start pos in the nums1 slice
-			appended = mergeAppend(appended, nums1, nums2)
-		} else if listRanges["n2end"] < listRanges["n1start"] {
-			fmt.Println("branch 2: n2 -> n1")
-			// the lists overlap and need to be merged in the opposite order
-			// TODO reverse the logic from above
-			appended = mergeAppend(appended, nums2, nums1)
-		} else if listRanges["n2start"] > listRanges["n1end"] {
-			// the lists can be appended num1s first
-			appended = append(nums1, nums2...)
-		} else {
-			// the lists can be appended num2s first
-			appended = append(nums2, nums1...)
-		}
+	// at this point we know the two arrays will be interwoven
+	// not which direction. first we need to find the order,
+	// 	then the center element of (len(nums1) + len(nums2))/2
+	if nums2[0] >= nums1[0] { // order num1..nums2
+		return medianFromLists(nums1, nums2)
+	} else if nums1[0] >= nums2[0] {
+		return medianFromLists(nums2, nums1)
+	} else {
+		panic("unreachable")
 	}
-
-	fmt.Println("appended array", appended)
-	// at this point the lists should be appended and median can be computed
-	var center float64 = float64(len(appended)) / 2.0
-	if len(appended)%2 == 0 {
-		center -= 0.5
-		sum := appended[int(center-0.5)] + appended[int(center+0.5)]
-		average := float64(sum) / 2.0
-		fmt.Println("sum", sum, "average", average)
-		return average
-	}
-
-	return float64(appended[int(center+0.5)])
 }
 
-func mergeAppend(appended []int, nums1 []int, nums2 []int) []int {
-	fmt.Println("getStartPos nums1", nums1, "target", nums2[0], "")
-	stepSize := len(nums1) / 4
-	if stepSize < 0 {
-		stepSize = 1
+func median(nums []int) float64 {
+	if len(nums) == 1 {
+		return float64(nums[0])
 	}
-	nums1PosN2Start := getStartPos(nums1, nums2[0], len(nums1)/2, stepSize)
-	if nums1PosN2Start > 0 {
-		if n := copy(appended, nums1[:nums1PosN2Start]); n < nums1PosN2Start+1 {
-			panic("unthinkable: the copy operation failed")
-		}
+	center := float64(len(nums)) / 2.0
+	// fmt.Printf("center %f\n", center)
+	if len(nums)%2 == 0 && len(nums) > 1 {
+		right := int64(math.Ceil(center))
+		left := right - 1
+		return (float64(nums[left]) + float64(nums[right])) / float64(2)
 	}
+	return float64(nums[int64(math.Floor(center))])
+}
 
-	// wherever n1 ends they stop overlapping, so increment
-	//  from nums1PosN2Start+1 .. getStartPos(nums2, listRanges["n1end"])
-	fmt.Println("getStartPos nums2", nums2, "n2end", nums2[len(nums2)-1])
-	nums2PosN1End := getStartPos(nums1, nums2[len(nums2)-1], len(nums2)/2, len(nums2)/4)
-	nums1Idx := nums1PosN2Start + 1 // TODO is this off by one?
-	// keep another increment on n2 so that the position when you break
-	//  out of a for true loop is the start of the rest of that slice that
-	//  needs to be appended
-	nums2Idx := 0
-	// const iterLimit = 1000
-	// i := 0
-	for true {
-		// i++
-		// if i > iterLimit {
-		// 	break
-		// }
-		nums1Val := nums1[nums1Idx]
-		nums2Val := nums2[nums2Idx]
-		if nums1Val < nums2Val {
-			appended = append(appended, nums1Val)
-			nums1Idx++
-		} else if nums1Val == nums2Val {
-			appended = append(appended, nums1Val, nums2Val)
-			nums1Idx++
-			nums2Idx++
-		} else {
-			// deductively nums1Val > nums2Val
-			appended = append(appended, nums2Val)
-			nums2Idx++
-		}
-		fmt.Println("nums1 current index", nums1Idx, "value", nums1Val,
-			"nums2 index", nums2Idx, "value", nums2Val)
-		if nums2Idx > nums2PosN1End {
+func medianFromLists(lower, upper []int) float64 {
+	totalLength := len(lower) + len(upper)
+	center := float64(totalLength) / 2.0
+
+	i, j := 0, 0
+	var lastV, curV int
+	cIdx := 0
+	for {
+		if float64(cIdx) > center {
 			break
 		}
-	}
+		lastV = maxOf(lastV, curV)
+		// if both values exist, compare and select
+		if i < len(lower) && j < len(upper) {
+			if lower[i] < upper[j] {
+				curV = lower[i]
+				i++
+			} else {
+				curV = upper[j]
+				j++
+			}
+			//fmt.Printf("curV %d, i %d, j %d\n", curV, i, j)
+		} else {
+			// one or the other doesn't exist, use the existing
+			if j < len(upper) {
+				curV = upper[j]
+				j++
+			} else if i < len(lower) {
+				curV = lower[i]
+				i++
+			}
+		}
 
-	appended = append(appended, nums2[nums1Idx:]...)
-	return appended
+		cIdx++
+		// fmt.Printf("lastV %d, curV %d\n", lastV, curV)
+	}
+	if (len(lower)+len(upper))%2 == 0 {
+		avg := (float64(lastV) + float64(curV)) / 2.0
+		// fmt.Printf("average %f\n", avg)
+
+		return avg
+	}
+	return float64(curV)
+
 }
 
-func getStartPos(nums []int, target, curIdx, stepSize int) int {
-	// get the position index before the target value
-	// assumes that nums are sorted
-
-	stepSizeHalved := stepSize / 2
-	if stepSize < 2 {
-		stepSizeHalved = 1
+func maxOf(a, b int) int {
+	if a > b {
+		return a
 	}
-	stepUp := curIdx + stepSize
-	stepDown := curIdx - stepSize
-	if stepUp > len(nums)-1 {
-		//stepUp = len(nums) - 1
-		start := len(nums) - 1
-		if start < 0 {
-			start = 0
-		}
-		return start
-	}
-	if stepDown < 0 {
-		//stepDown = 0
-		return 0
-	}
-	if nums[curIdx] > target && nums[curIdx-1] <= target {
-		fmt.Println("gPos", curIdx, nums[curIdx], "gpos-1",
-			nums[curIdx-1], "target", target)
-		return curIdx - 1
-	} else if nums[curIdx] > target {
-		return getStartPos(nums, target, stepDown, stepSizeHalved)
-	} else {
-		return getStartPos(nums, target, curIdx+stepSize, stepSizeHalved)
-	}
-
+	return b
 }
--- hard/4_MedianOfSortedLists_test.go
+++ hard/4_MedianOfSortedLists_test.go
@@ -1,7 +1,7 @@
 package hard
 
 import (
-	"reflect"
+	"fmt"
 	"testing"
 )
 
@@ -16,22 +16,22 @@ func Test_findMedianSortedArrays(t *testing.T) {
 		want float64
 	}{
 		// TODO: Add test cases.
-		// {
-		// 	name: "Example 1",
-		// 	args: args{
-		// 		nums1: []int{1, 3},
-		// 		nums2: []int{2},
-		// 	},
-		// 	want: 2,
-		// },
-		// {
-		// 	name: "Example 2",
-		// 	args: args{
-		// 		nums1: []int{1, 2},
-		// 		nums2: []int{3, 4},
-		// 	},
-		// 	want: 2.5,
-		// },
+		{
+			name: "Example 1",
+			args: args{
+				nums1: []int{1, 3},
+				nums2: []int{2},
+			},
+			want: 2,
+		},
+		{
+			name: "Example 2",
+			args: args{
+				nums1: []int{1, 2},
+				nums2: []int{3, 4},
+			},
+			want: 2.5,
+		},
 		{
 			name: "just nums1",
 			args: args{
@@ -48,146 +48,45 @@ func Test_findMedianSortedArrays(t *testing.T) {
 			},
 			want: 2.5,
 		},
-		// {
-		// 	name: "Edge Case 1",
-		// 	args: args{
-		// 		nums1: []int{},
-		// 		nums2: []int{},
-		// 	},
-		// 	want: 0,
-		// },
+		{
+			name: "Example 5",
+			args: args{
+				nums1: []int{1, 3, 5},
+				nums2: []int{2},
+			},
+			want: 2.5,
+		},
+		{
+			name: "Edge Case 1",
+			args: args{
+				nums1: []int{},
+				nums2: []int{1},
+			},
+			want: 1,
+		},
+		{
+			name: "Edge Case 2",
+			args: args{
+				nums1: []int{1, 3},
+				nums2: []int{2, 7},
+			},
+			want: 2.5,
+		},
+		{
+			name: "Edge Case 3",
+			args: args{
+				nums1: []int{3},
+				nums2: []int{-2, -1},
+			},
+			want: -1,
+		},
 	}
 	for _, tt := range tests {
+		fmt.Printf("test name %s\n", tt.name)
 		t.Run(tt.name, func(t *testing.T) {
 			if got := findMedianSortedArrays(tt.args.nums1,
 				tt.args.nums2); got != tt.want {
 				t.Errorf("findMedianSortedArrays() = %v, want %v", got, tt.want)
-			}
-		})
-	}
-}
-
-func Test_getStartPos(t *testing.T) {
-	type args struct {
-		nums     []int
-		target   int
-		curIdx   int
-		stepSize int
-	}
-	tests := []struct {
-		name string
-		args args
-		want int
-	}{
-		// TODO: Add test cases.
-		{
-			name: "simple sequence",
-			args: args{
-				nums:     []int{1, 2, 3, 4, 5, 6},
-				target:   2,
-				curIdx:   2,
-				stepSize: 1,
-			},
-			want: 1,
-		},
-		{
-			name: "hole in sequence",
-			args: args{
-				nums:     []int{1, 2, 3, 9, 14, 15, 16},
-				target:   7,
-				curIdx:   2,
-				stepSize: 1,
-			},
-			want: 2,
-		},
-		{
-			name: "negative numbers",
-			args: args{
-				nums:     []int{-17, -14, -11, -9, -3, 0, 1},
-				target:   -12,
-				curIdx:   2,
-				stepSize: 1,
-			},
-			want: 1,
-		},
-		{
-			name: "edge case 1",
-			args: args{
-				nums:     []int{1, 3},
-				target:   2,
-				curIdx:   0,
-				stepSize: 1,
-			},
-			want: 0,
-		},
-		{
-			name: "edge case stack overflow",
-			args: args{
-				nums:     []int{4, 6, 8, 9},
-				target:   1,
-				curIdx:   0,
-				stepSize: 1,
-			},
-			want: 0,
-		},
-		{
-			name: "",
-			args: args{
-				nums:     []int{},
-				target:   0,
-				curIdx:   0,
-				stepSize: 1,
-			},
-			want: 0,
-		},
-		// {
-		// 	name: "empty input", // this should not be possible, so
-		// 	args: args{			// I'm going to let it panic
-		// 		nums:   []int{},
-		// 		target: 0,
-		// 		curIdx: 0,
-		// 		stepSize: 1,
-		// 	},
-		// 	want: -math.MaxInt,
-		// },
-	}
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			if got := getStartPos(tt.args.nums, tt.args.target,
-				tt.args.curIdx, tt.args.stepSize); got != tt.want {
-				t.Errorf("getStartPos() = %v, want %v", got, tt.want)
-			}
-		})
-	}
-}
-
-func Test_mergeAppend(t *testing.T) {
-	type args struct {
-		appended []int
-		nums1    []int
-		nums2    []int
-	}
-	tests := []struct {
-		name string
-		args args
-		want []int
-	}{
-		// TODO: Add test cases.
-		{
-			name: "merge starting with second array",
-			args: args{
-				appended: []int{},
-				nums1:    []int{4, 6, 8, 9},
-				nums2:    []int{1, 2, 5, 7},
-			},
-			want: []int{1, 2, 3, 4, 5, 6, 7, 8},
-		},
-	}
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			if got := mergeAppend(tt.args.appended, tt.args.nums1,
-				tt.args.nums2); !reflect.DeepEqual(got, tt.want) {
-				t.Errorf("mergeAppend() = %v, want %v", got, tt.want)
 			}
 		})
 	}
