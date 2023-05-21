a78a2c8d02475a329440b1e45090a158b1491a4a
Sam Myres <samiam2013@gmail.com>
2023-05-21 17:04:11 -0500

    add missing solution

--- easy/27_RemoveElement.go
+++ easy/27_RemoveElement.go
@@ -1,5 +1,17 @@
 package easy
 
 func removeElement(nums []int, val int) int {
-	return 0
+	if len(nums) == 0 {
+		return 0
+	}
+
+	writeIdx := 0
+	for i := 0; i < len(nums); i++ {
+		if nums[i] == val {
+			continue
+		}
+		nums[writeIdx] = nums[i]
+		writeIdx++
+	}
+	return writeIdx
 }
